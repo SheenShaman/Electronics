d523d0caa06a07dcca8a3cc266d57ceefbeb44b4
Danil Sheen <dan.80956@gmail.com>
2024-01-23 21:28:19 +0300

    add main urls

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import MainView
+from main.views import LinkCreateAPIView, LinkListAPIView, LinkRetrieveAPIView, LinkUpdateAPIView, LinkDestroyAPIView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', MainView.as_view(), name='main'),
+    path('link/create/', LinkCreateAPIView.as_view(), name='link_create'),
+    path('links/', LinkListAPIView.as_view(), name='link_list'),
+    path('link/<int:pk>/', LinkRetrieveAPIView.as_view(), name='link_get'),
+    path('link/update/<int:pk>/', LinkUpdateAPIView.as_view(), name='link_update'),
+    path('link/delete/<int:pk>/', LinkDestroyAPIView.as_view(), name='link_delete'),
 ]
