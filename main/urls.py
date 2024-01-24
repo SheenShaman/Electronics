40820342323be6dff1b1015d4f0b85c9388d91ae
Danil Sheen <dan.80956@gmail.com>
2024-01-24 20:52:07 +0300

    changed main urls

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from main.apps import MainConfig
-from main.views import LinkCreateAPIView, LinkListAPIView, LinkRetrieveAPIView, LinkUpdateAPIView, LinkDestroyAPIView
+from main.views import (LinkCreateAPIView, LinkListAPIView, LinkRetrieveAPIView, LinkUpdateAPIView, LinkDestroyAPIView,
+                        ContactsViewSet, ProductViewSet)
 
 app_name = MainConfig.name
+
+router = DefaultRouter()
+router.register(r'contacts', ContactsViewSet, basename='contacts')
+router.register(r'products', ProductViewSet, basename='products')
 
 urlpatterns = [
     path('link/create/', LinkCreateAPIView.as_view(), name='link_create'),
@@ -11,4 +17,4 @@ urlpatterns = [
     path('link/<int:pk>/', LinkRetrieveAPIView.as_view(), name='link_get'),
     path('link/update/<int:pk>/', LinkUpdateAPIView.as_view(), name='link_update'),
     path('link/delete/<int:pk>/', LinkDestroyAPIView.as_view(), name='link_delete'),
-]
+] + router.urls
