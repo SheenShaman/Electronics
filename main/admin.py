b668ae2a01ce18ad94a249596797a87449219b4b
Danil Sheen <dan.80956@gmail.com>
2024-01-24 21:43:49 +0300

    added reference on provider and admin-action 'clear debt'

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from main.models import Link, Contacts, Product
 
@@ -10,8 +12,23 @@ class ContactsAdmin(admin.ModelAdmin):
 
 @admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
-    list_display = ('name', 'contacts', 'provider', 'debt', 'created_at',)
+    list_display = ('name', 'contacts', 'get_providers', 'debt', 'created_at',)
     list_filter = ('contacts__city',)
+    actions = ('clear_debt',)
+    list_display_links = ('get_providers',)
+
+    def get_providers(self, obj):
+        provider = obj.provider
+        if provider:
+            url = reverse('admin:main_link_change', args=[provider.id])
+            return format_html('<a href="{}">{}</a>', url, provider.name)
+        return '-'
+
+    get_providers.short_description = 'Поставщик'
+
+    @admin.action(description='Обнулить задолжность')
+    def clear_debt(self, _, queryset):
+        queryset.update(debt=0.00)
 
 
 @admin.register(Product)
