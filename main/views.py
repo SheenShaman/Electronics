7e512dc3a90051533eae4c250b99eadec7b34d78
Danil Sheen <dan.80956@gmail.com>
2024-01-24 20:48:43 +0300

    changed main views

--- main/views.py
+++ main/views.py
@@ -1,38 +1,66 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
 
-from main.models import Link
-from main.serializers import LinkSerializer
+from main.models import Link, Contacts, Product
+from main.serializers import LinkSerializer, ContactsSerializer, ProductSerializer
+
+
+class ContactsViewSet(ModelViewSet):
+    """ ViewSet for Contacts """
+    serializer_class = ContactsSerializer
+    queryset = Contacts.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+
+class ProductViewSet(ModelViewSet):
+    """ ViewSet for Products """
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
 
 
 class LinkCreateAPIView(generics.CreateAPIView):
     """ Create Link """
     serializer_class = LinkSerializer
-    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class LinkListAPIView(generics.ListAPIView):
     """ Get Links """
     serializer_class = LinkSerializer
     queryset = Link.objects.all()
-    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ['contacts__country']
 
 
 class LinkRetrieveAPIView(generics.RetrieveAPIView):
     """ Get Link """
     serializer_class = LinkSerializer
     queryset = Link.objects.all()
-    permission_classes = [IsAuthenticated]
 
 
 class LinkUpdateAPIView(generics.UpdateAPIView):
     """ Update Link """
     serializer_class = LinkSerializer
     queryset = Link.objects.all()
-    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        serializer.save()
 
 
 class LinkDestroyAPIView(generics.DestroyAPIView):
     """ Delete Link """
     queryset = Link.objects.all()
-    permission_classes = [IsAuthenticated]
