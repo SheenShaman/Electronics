269face6598f9d55cd5552db209fe369e02205dc
Danil Sheen <dan.80956@gmail.com>
2024-01-24 22:34:33 +0300

    added ViewSet for User

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    """ ViewSet for Users """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
