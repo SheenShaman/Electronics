403271105890f3ebaba2ccb2c67c56efed8ccdab
Danil Sheen <dan.80956@gmail.com>
2024-01-24 20:48:11 +0300

    changed main serializers

--- main/serializers.py
+++ main/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from main.models import Link
+from main.models import Link, Contacts, Product
+
+
+class ContactsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contacts
+        fields = '__all__'
 
 
 class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
         fields = '__all__'
