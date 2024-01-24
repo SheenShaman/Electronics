36baf5705114a19edfd01f3d022f29554f007842
Danil Sheen <dan.80956@gmail.com>
2024-01-24 20:47:51 +0300

    changed main admin

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from main.models import Link
+from main.models import Link, Contacts, Product
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'country', 'city', 'street', 'house_number',)
 
 
 @admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'provider', 'debt', 'created_at')
-    list_filter = ('city', )
+    list_display = ('name', 'contacts', 'provider', 'debt', 'created_at',)
+    list_filter = ('contacts__city',)
+
+
+@admin.register(Product)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('product_title', 'model', 'product_launch_date',)
